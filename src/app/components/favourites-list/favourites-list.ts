6fa7f9eee89b45fc224ccaab8422f174c9a2ae05
Pantelis Papasavvas <pantpap@gmail.com>
2025-10-18 23:45:49 +0300

    feat(favorites-list): added favorites list

--- src/app/components/favourites-list/favourites-list.ts
+++ src/app/components/favourites-list/favourites-list.ts
@@ -1,12 +1,27 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, signal} from '@angular/core';
+import {Photo} from '../../interfaces/photo';
+import {NgOptimizedImage} from '@angular/common';
+import {Router} from '@angular/router';
+import {Favorites} from '../../services/favorites/favorites';
 
 @Component({
   selector: 'app-favourites-list',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './favourites-list.html',
   styleUrl: './favourites-list.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FavouritesList {
 
+  private readonly favoriteService = inject(Favorites);
+  private readonly router = inject(Router);
+
+  favoritesPhotos = signal<Photo[]>(this.favoriteService.getFavorites())
+
+  viewPhoto(photo: Photo) {
+    this.favoriteService.setSelectedPhoto(photo);
+    this.router.navigateByUrl(`/photos/${photo.id}`);
+  }
 }
