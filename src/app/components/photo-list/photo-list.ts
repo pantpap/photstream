4944c6424e5ff0c89869de56f6c00616d1364416
Pantelis Papasavvas <pantpap@gmail.com>
2025-10-18 18:00:50 +0300

    feat(photo-list): display images in grid

--- src/app/components/photo-list/photo-list.ts
+++ src/app/components/photo-list/photo-list.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, signal} from '@angular/core';
+import { NgOptimizedImage} from '@angular/common';
+import {FetchPhotoList} from '../../services/fetch-photo-list';
+import {Photo} from '../../interfaces/photo';
+import {rxResource, toSignal} from '@angular/core/rxjs-interop';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
+import {InfiniteScroll} from '../../directives/infinite-scroll';
 
 @Component({
   selector: 'app-photo-list',
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    MatProgressSpinner,
+    InfiniteScroll,
+  ],
   templateUrl: './photo-list.html',
-  styleUrl: './photo-list.scss'
+  styleUrl: './photo-list.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PhotoList {
+  private readonly photoService = inject(FetchPhotoList);
 
+  error = signal<unknown>(null);
+  page = signal(1);
+  pageSize = signal(30);
+
+
+  photosResource = rxResource({
+    params: () => ({ page: this.page(), size: this.pageSize() }),
+    stream: () => {
+      return this.photoService.getPhotos(this.pageSize());
+    },
+  });
+
+  loadMore() {
+    this.page.update(p => p + 1);
+  }
+
+  selectedPhoto(photo: Photo) {
+
+  }
 }
---
+++ src/app/interfaces/photo.ts
@@ -0,0 +1,7 @@
+export interface Photo {
+  id: number;
+  thumbUrl: string,
+  download_url: string,
+  width: number,
+  height: number,
+}
