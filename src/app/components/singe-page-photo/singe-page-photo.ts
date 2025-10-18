d4fe481dcef9af6e648b8a077d83f182fab0f44a
Pantelis Papasavvas <pantpap@gmail.com>
2025-10-18 23:46:24 +0300

    feat(single-page): added single-page-photo component

--- src/app/components/singe-page-photo/singe-page-photo.ts
+++ src/app/components/singe-page-photo/singe-page-photo.ts
@@ -1,12 +1,42 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, inject, signal} from '@angular/core';
+import {Favorites} from '../../services/favorites/favorites';
+import {JsonPipe, NgOptimizedImage} from '@angular/common';
+import {MatFabButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+import {Router, RouterLink, RouterLinkActive} from '@angular/router';
+import {LocalStorage} from '../../services/local-storage/local-storage';
+import {FAV_PHOTOS} from '../../constants';
+import {Photo} from '../../interfaces/photo';
 
 @Component({
   selector: 'app-singe-page-photo',
-  imports: [],
+  imports: [
+    JsonPipe,
+    NgOptimizedImage,
+    MatFabButton,
+    MatIcon,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './singe-page-photo.html',
   styleUrl: './singe-page-photo.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SingePagePhoto {
 
+  private readonly favoriteService = inject(Favorites);
+  private readonly localStorageService = inject(LocalStorage);
+  private readonly router = inject(Router);
+
+  favoritePhoto = signal(this.favoriteService.getSelectedPhoto());
+
+  readonly favoriteUrl = computed(() => this.favoritePhoto()?.download_url ?? '');
+
+  removeFromFavorites(id: number) {
+    const favoritesList = this.favoriteService.getFavorites();
+    const updatedList = favoritesList.filter((photo: Photo) => photo.id !== id);
+    this.localStorageService.setItem(FAV_PHOTOS, JSON.stringify(updatedList));
+    this.favoritePhoto.set(null);
+    this.router.navigateByUrl(`/favorites`);
+  }
 }
